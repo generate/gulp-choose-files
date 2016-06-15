f54c6aef4fcee97d89e02407f53b274dd0162803
jonschlinkert <jon.schlinkert@sellside.com>
2016-06-15 18:04:24 -0400

    allow prompt to be skipped

--- index.js
+++ index.js
@@ -5,21 +5,39 @@ var extend = require('extend-shallow');
 var Questions = require('question-cache');
 
 module.exports = function(options) {
-  var opts = extend({prop: 'relative'}, options);
+  var opts = extend({key: 'relative', save: false}, options);
   var msg = opts.message || 'Which files do you want to write?';
   var questions = new Questions();
   var paths = [];
   var files = {};
 
   return through.obj(function(file, enc, next) {
+    if (opts.skip) {
+      next(null, file);
+      return;
+    }
+
     var key = fileKey(file, opts);
     paths.push(key);
     files[key] = file;
     next();
   }, function(next) {
     var stream = this;
+
+    if (typeof opts.choices === 'string') {
+      opts.choices = [opts.choices];
+    }
+
+    if (Array.isArray(opts.choices)) {
+      opts.choices.forEach(function(filepath) {
+        stream.push(files[filepath]);
+      });
+      next();
+      return;
+    }
+
     questions.choices('files', msg, paths);
-    questions.ask('files', function(err, answers) {
+    questions.ask('files', opts, function(err, answers) {
       if (err || !answers.files) {
         next(err);
         return;
